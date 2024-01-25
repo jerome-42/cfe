f270a3a083059252427a2d1b8e02ca149c15917c
AAVO <info@aavo.org>
2024-01-25 21:11:55 +0000

    check date

--- js/declaration.js
+++ js/declaration.js
@@ -1,4 +1,23 @@
+var nextWithPostData = function(url, params) {
+    var form = document.createElement("form");
+    form.setAttribute("method", "POST");
+    form.setAttribute("accept-charset", "UTF-8");
+    form.setAttribute("action", url);
 
+    for(var key in params) {
+        if(params.hasOwnProperty(key)) {
+            var hiddenField = document.createElement("input");
+            hiddenField.setAttribute("type", "hidden");
+            hiddenField.setAttribute("name", key);
+            hiddenField.setAttribute("value", params[key]);
+
+            form.appendChild(hiddenField);
+        }
+    }
+
+    document.body.appendChild(form);
+    form.submit();
+};
 
 $(document).ready(function() {
     $('#dateCFE').focus();
@@ -6,14 +25,27 @@ $(document).ready(function() {
 	history.back();
 	return event.preventDefault();
     });
-    $('#doRecord').click(function() {
-	$('#newRec').submit();
+    $('form').on('submit', function(event) {
+	event.preventDefault();
     });
-    $('newRec').on('submit', function(event) {
-	// on vérifie ici les entrées
+    $('#doRecord').click(function() {
+	// on vérifie ici les entrées de l'utilisateur avant d'envoyer le formulaire au serveur
+
 	$('.invalid-feedback').remove();
 
-	// TODO check date
+	// check date
+	var date = $('#dateCFE').val();
+	if (date === '') {
+	    $('#dateCFE').after($('<div class="invalid-feedback">').text('Date obligatoire'));
+	} else {
+	    var now = new Date();
+	    date = new Date(date);
+	    if (date.getFullYear() != now.getFullYear()) { // l'année doit être l'année en cours
+		$('#dateCFE').after($('<div class="invalid-feedback">').text(date.getFullYear()+" n'est pas une année correcte"));
+	    } else if (date.getTime() > now.getTime()) { // la date saisie est après aujourd'hui !
+		$('#dateCFE').after($('<div class="invalid-feedback">').text("Impossible de pré-déclarer !"));
+	    }
+	}
 
 	// DUREE
 	// on remplace les virgules par des . (notation française versus anglaise)
@@ -30,11 +62,12 @@ $(document).ready(function() {
 	    $('#duree').after($('<div class="invalid-feedback">').text('Impossible de saisir plus de 10 heures').css('display', ''));
 	}
 	// /DUREE
+
+	// on affiche toutes les erreurs
 	$('.invalid-feedback').css({ 'display': 'initial' });
-	if ($('.invalid-feedback').length > 0) {
-	    // il y a des erreurs, on n'envoie pas le formulaire
-	    return event.preventDefault();
+	if ($('.invalid-feedback').length === 0) {
+	    // il n'y a pas d'erreur, on envoie le formulaire
+	    return nextWithPostData('/declaration', { dateCFE: date, duree: duree, type: $('#type').val(), beneficiaire: $('#beneficiaire').val(), commentaires: $('#commentaires').val() });
 	}
-	// pas d'erreur, on envoie le formulaire
     });
 });
