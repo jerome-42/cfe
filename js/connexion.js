afeddc9ccc01dd0de0f8dde4a2f73fba8f23f498
AAVO <info@aavo.org>
2024-01-25 09:11:18 +0000

    connexion plus joli

--- js/connexion.js
+++ js/connexion.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
     $('input[name="login"]').focus();
+    $('form').on('submit', function(event) {
+	$('.alert').remove();
+	$('button[type="submit"]').remove();
+	$('#spinner').removeClass('visually-hidden');
+    });
 });
