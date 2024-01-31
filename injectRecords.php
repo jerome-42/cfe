c905b8f6144e88a0c2b04efca70789e4dbba01ba
AAVO <info@aavo.org>
2024-01-31 13:29:25 +0000

    pour ceux qui ne savent pas tapper leur nom !

--- injectRecords.php
+++ injectRecords.php
@@ -5,6 +5,10 @@
 function parseDateDDMMAAAA($date) {
     $d = explode(' ', $date);
     $elem = explode('/', $d[0]);
+    if ($elem[2] === '0023') // pour ceux qui ne savent pas saisir une date !
+        $elem[2] = '2023';
+    if ($elem[1] > 12) // pour ceux qui ne savent pas saisir une date !
+        return parseDateMMDDAAAA($date);
     if (count($d) == 1)
         return ['year' => $elem[2], 'month' => $elem[1], 'day' => $elem[0] ];
     else
@@ -16,6 +20,8 @@ function parseDateDDMMAAAA($date) {
 function parseDateMMDDAAAA($date) {
     $d = explode(' ', $date);
     $elem = explode('/', $d[0]);
+    if ($elem[2] === '0023') // pour ceux qui ne savent pas saisir une date !
+        $elem[2] = '2023';
     if (count($d) == 1)
         return ['year' => $elem[2], 'month' => $elem[0], 'day' => $elem[1] ];
     else
@@ -32,13 +38,38 @@ function toDate($date, $withTime = true) {
 function getGivavNumFromName($conn, $name) {
     switch ($name) {
     case "Poulain Malo":
+    case "POULAIN Malo":
         return 31654;
     case "Boura Mmarc":
         return 644;
+    case "Gildas LE NAOUR":
+        return 40574;
+    case "Maoujoud M’hamed":
+        return 44413;
+    case "Porta-Penhouët Laurence":
+    case "PORTA-PENHOUËT Laurence":
+    case "PENHOUËT-PORTA Laurence":
+    case "PENHOUËT Laurence":
+        return 44971;
+    case "Leroux Roger.":
+        return 709;
+    case "Moutetde Pasca":
+    case "Moutetde Pascal":
+        return 720;
+    case "dietln michel":
+        return 7677;
+    case "Gainnet-herblot Luc":
+    case "Galland Pierre-Louis":
+    case "GALLAND Pierre-Louis":
+    case "LE DELLIOU Patrick":
+        return -1;
     }
     $query = "SELECT givavNumber FROM personnes WHERE REPLACE(LOWER(name), '-', ' ') LIKE :name LIMIT 1";
     $sth = $conn->prepare($query);
-    $sth->execute([ ':name' => '%'.str_replace('-', '%', strtolower($name)).'%' ]);
+    $correctedName = str_replace('-', '%', strtolower($name));
+    $correctedName = str_replace([ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ], '', $correctedName);
+    $correctedName = preg_replace('/\s+/', ' ', $correctedName);
+    $sth->execute([ ':name' => '%'.$correctedName.'%' ]);
     if ($sth->rowCount() === 1)
         return $sth->fetchAll()[0]['givavNumber'];
     throw new Exception("impossible de trouver le numéro givav de: ".$name);
@@ -67,10 +98,11 @@ if (($handle = fopen($argv[1], "r")) !== FALSE) {
             $data[$i] = trim($data[$i]);
         $data[0] = parseDateMMDDAAAA($data[0]); // registerDate
         $data[7] = parseDateDDMMAAAA($data[7]); // workDate
-        if ($data[7]['year'] != '2024')
-            continue;
-        //DEBUG echo "row: ".$row.PHP_EOL;
-        //DEBUG var_dump($data);
+        // on prend toutes les dates
+        //if ($data[7]['year'] != '2024')
+        //    continue;
+        echo "row: ".$row.PHP_EOL;
+        var_dump($data);
         $details = [];
         foreach ([ 5, 8 ] as $column) {
             if ($data[$column] !== '')
@@ -81,7 +113,10 @@ if (($handle = fopen($argv[1], "r")) !== FALSE) {
             if ($data[$column] != '')
                 $name[] = $data[$column];
         }
-        $sth->execute([ ':who' => getGivavNumFromName($conn, implode(' ', $name)),
+        $who = getGivavNumFromName($conn, implode(' ', $name));
+        if ($who === -1) // le membre n'est pas inscrit
+            continue;
+        $sth->execute([ ':who' => $who,
                         ':registerDate' => toDate($data[0]),
                         ':workDate' => toDate($data[7], false),
                         ':workType' => $data[3],
