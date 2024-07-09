1d6537ad8d8186a58e9b7f11554bdc307e071b9d
Jérôme Marteaux <jerome@dedwen.info>
2024-07-09 13:57:30 +0000

    authentification

--- routes.php
+++ routes.php
@@ -1145,8 +1145,21 @@ get('/robots.txt', function($conn) {
 });
 
 get('/tableau-de-bord', function($conn, $pug) {
-    if (!isset($_SESSION['auth']) || $_SESSION['isAdmin'] === false)
-        return redirect('/');
+    // authentification par http basic, un peu moche, il aurait été meilleur de faire un middleware
+    file_put_contents('log', json_encode($_SERVER).PHP_EOL, FILE_APPEND);
+    if (!isset($_SERVER['PHP_AUTH_USER'])) {
+        header('WWW-Authenticate: Basic realm="Tableau de bord"');
+        header('HTTP/1.0 401 Unauthorized');
+        echo "Partie protégée par un mot de passe";
+        return;
+    } else {
+        if ($_SERVER['PHP_AUTH_USER'] !== 'tableau' || $_SERVER['PHP_AUTH_PW'] !== 'cherence') {
+            header('WWW-Authenticate: Basic realm="Tableau de bord"');
+            header('HTTP/1.0 401 Unauthorized');
+            echo "Partie protégée par un mot de passe";
+            return;
+        }
+    }
     $pug->displayFile('view/tableau-de-bord.pug', $_SESSION);
 });
 
