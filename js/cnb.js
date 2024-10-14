1e4567a765d65a6b01548b69cb95a0d5b320c131
Jérôme Marteaux <jerome@dedwen.info>
2024-10-14 20:48:54 +0000

    cnb

--- js/cnb.js
+++ js/cnb.js
@@ -2,4 +2,17 @@ $(document).ready(function() {
     $('.back').click(function() {
 	window.location = '/';
     });
+    stats.CNB.data.forEach(function(l) {
+        let tr = $('<tr>')
+            .append($('<td>').text(l.immatriculation))
+            .append($('<td>').text(l.nom_type))
+            .append($('<td>').text(l.temps_vol_hors_proprietaire));
+        let color = 'orange';
+        if (l.temps_cnb_a_realiser == '00:00:00')
+            color = 'green';
+        else if (l.temps_cnb_a_realiser == stats.CNB.params[':cnb'])
+            color = 'red';
+        tr.append($('<td>', { style: 'color: '+color }).text(l.temps_cnb_a_realiser));
+        $('#list > tbody').append(tr);
+    });
 });
